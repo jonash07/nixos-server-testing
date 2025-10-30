2711a81a4354aaa0d9af9ab2c89b04b054a78d76
jonash07 <jonash07@proton.me>
2025-10-30 20:54:44 +0100

    stupidity

--- modules/filesystems.nix
+++ modules/filesystems.nix
@@ -4,7 +4,7 @@
   fileSystems."/storage" = {
     device = "/dev/disk/by-uuid/ae6c8d55-84ff-43c6-8c3e-f2072dbbe6b6";
     fsType = "btrfs";
-    options = [ "noatime" "compression=zstd" ];
+    options = [ "noatime" "compress=zstd" ];
   };
 }
 
