172292626603f11b3cab65d2f9372d0721f81c93
jonash07 <jonash07@proton.me>
2025-10-30 19:48:55 +0100

    fuck me

--- modules/environment.nix
+++ modules/environment.nix
@@ -6,9 +6,9 @@
   };
 
   environment.shellAliases = {
-    snrs = "sudo nixos-rebuild switch --flake ~/nixos-server-testing#server";
-    snrb = "sudo nixos-rebuild boot --flake ~/nixos-server-testing#server";
-    snrt = "sudo nixos-rebuild test --flake ~/nixos-server-testing#server";
+    snrs = "sudo nixos-rebuild switch --flake ~/server-nixos#server";
+    snrb = "sudo nixos-rebuild boot --flake ~/server-nixos#server";
+    snrt = "sudo nixos-rebuild test --flake ~/server-nixos#server";
     nfu = "sudo nix flake update --flake ~/server-nixos";
     ncg = "sudo nix-collect-garbage --delete-older-than 1d";
   };
