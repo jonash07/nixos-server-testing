b0327b510873afd63ba054d4fcf6669036b38c04
jonash07 <jonash07@proton.me>
2025-10-30 19:47:06 +0100

    bleeh

--- modules/environment.nix
+++ modules/environment.nix
@@ -9,7 +9,7 @@
     snrs = "sudo nixos-rebuild switch --flake ~/nixos-server-testing#server";
     snrb = "sudo nixos-rebuild boot --flake ~/nixos-server-testing#server";
     snrt = "sudo nixos-rebuild test --flake ~/nixos-server-testing#server";
-    nfu = "sudo nix flake update --flake ~/nixos-config";
+    nfu = "sudo nix flake update --flake ~/server-nixos";
     ncg = "sudo nix-collect-garbage --delete-older-than 1d";
   };
 
