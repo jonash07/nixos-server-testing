45e8358fc547ba840e35bbdd11f88c9aa7109fde
jonash07 <jonash07@proton.me>
2025-12-22 15:51:27 +0100

    hardware acceleration for immich

--- modules/immich.nix
+++ modules/immich.nix
@@ -6,7 +6,10 @@
     openFirewall = true;
     host = "0.0.0.0"; # fix for some stupid ass network problem immich created
     mediaLocation = "/external/storage/immich";
+    accelerationDevices = null;
   };
+
+  users.users.immich.extraGroups = [ "video" "render" ];
 
 }
 
