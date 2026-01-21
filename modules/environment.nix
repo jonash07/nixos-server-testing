22d302f57396f0cef19c441944cbf31318bb67cc
jonash07 <jonash07@proton.me>
2026-01-21 12:55:28 +0100

    limits cores because rebuild eats the entire processor

--- modules/environment.nix
+++ modules/environment.nix
@@ -6,9 +6,9 @@
   };
 
   environment.shellAliases = {
-    nrs = "sudo nixos-rebuild switch --flake ~/server-nixos#server";
-    nrb = "sudo nixos-rebuild boot --flake ~/server-nixos#server";
-    nrt = "sudo nixos-rebuild test --flake ~/server-nixos#server";
+    nrs = "sudo nixos-rebuild switch --cores 3 --flake ~/server-nixos#server";
+    nrb = "sudo nixos-rebuild boot --cores 3 --flake ~/server-nixos#server";
+    nrt = "sudo nixos-rebuild test --cores 3 --flake ~/server-nixos#server";
     nfu = "sudo nix flake update --flake ~/server-nixos";
     ncg = "sudo nix-collect-garbage --delete-older-than 1d";
   };
